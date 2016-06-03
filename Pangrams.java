fbe32b6976e03cc042c7c5b66979ac7773556c93
Darren Pearson <darrenpearson@comcast.net>
2016-06-03 13:10:40 -0600

    Add Pangrams

---
+++ Pangrams.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Arrays;
+
+class Pangrams {
+
+	static boolean isPangram(String sentence){
+
+		boolean[] letters = new boolean[26];
+
+		char[] temp = sentence.toUpperCase().replaceAll("[^A-Z]","").toCharArray();
+		for (char c: temp) {
+			letters[c - 'A'] = true;
+		}
+
+		for (boolean b: letters) {
+			if (!b) return false;
+		}
+
+		return true;
+	}
+}
