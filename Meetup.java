ec0d46560c85d27b92803204f2d13d47f84774d9
Darren Pearson <darrenpearson@comcast.net>
2016-06-04 10:49:04 -0600

    Improved conciseness

--- Meetup.java
+++ Meetup.java
@@ -5,54 +5,46 @@ public class Meetup {
 
 	private int month;
 	private int year;
+	private DateTime firstOfMonth;
 
 	public Meetup(int month, int year) {
 		this.month = month;
 		this.year = year;
+		firstOfMonth = new DateTime(this.year, this.month, 1, 0, 0);
 	}
 
 	public DateTime day(int day_of_week, MeetupSchedule schedule) {
-
+		DateTime firstOccurrence = findFirstDayOfWeek(day_of_week);
 		switch (schedule) {
-			case FIRST:  return findDate(day_of_week, 0); 
-			case SECOND: return findDate(day_of_week, 1);
-			case THIRD:  return findDate(day_of_week, 2);
-			case FOURTH: return findDate(day_of_week, 3);
-			default:
+			case FIRST:  return firstOccurrence; 
+			case SECOND: return firstOccurrence.plusWeeks(1);
+			case THIRD:  return firstOccurrence.plusWeeks(2);
+			case FOURTH: return firstOccurrence.plusWeeks(3);
+			case LAST:   return findLastDayOfWeek(day_of_week);  
+			default:     return findTeenthDayOfWeek(firstOccurrence);
 		}
-
-		// case of the LAST
-		DateTime date = new DateTime(this.year, this.month, 1, 0, 0);
-		if (schedule == MeetupSchedule.LAST) {
-		    date = date.dayOfMonth()
-		           	    .withMaximumValue()
-		                .dayOfWeek()
-		                .setCopy(day_of_week);
-		    if (date.getMonthOfYear() != month) {
-		    	return date.dayOfWeek().addToCopy(-7);
-		    }
-	    return date;
-		}
-		
-		// Default case of the TEENTHS
-		date = findDate(day_of_week, 0);
-		while (date.getDayOfMonth() < 13) {
-			date = date.dayOfWeek().addToCopy(7);
-		}
-		return date;
 	}
 	
-	/** Find date based on the first occurrence of the day_of_week in the month
-	 */
-	private DateTime findDate(int day_of_week, int week_adjust) {
-		DateTime date = new DateTime(this.year, this.month, 1, 0, 0);
-	    date = date.dayOfMonth()
-	           	    .withMinimumValue()
-	                .dayOfWeek()
-	                .setCopy(day_of_week);
-	    if (date.getMonthOfYear() < month) {
-	        date = date.dayOfWeek().addToCopy(7);
-	    }
-	    return date.dayOfWeek().addToCopy(week_adjust * 7);
+	private DateTime findFirstDayOfWeek(int day_of_week) {
+		DateTime date = firstOfMonth;
+		while (date.getDayOfWeek() != day_of_week) {
+			date = date.plusDays(1);
+		}
+	    return date;
+	}
+
+	private DateTime findLastDayOfWeek(int day_of_week) {
+		DateTime date = firstOfMonth.plusMonths(1).minusDays(1);
+		while (date.getDayOfWeek() != day_of_week) {
+			date = date.minusDays(1);
+		}
+	    return date;
+	}
+	
+	private DateTime findTeenthDayOfWeek(DateTime date) {
+		while (date.getDayOfMonth() < 13) {
+			date = date.plusWeeks(1);
+		}
+		return date;
 	}
 }
