01f4472e721393067da716050e293dd634c480e2
Darren Pearson <darrenpearson@comcast.net>
2016-06-07 14:26:42 -0600

    Updated to streams.

--- Octal.java
+++ Octal.java
@@ -8,17 +8,8 @@ public class Octal {
 
 	public int getDecimal() {
 		if (!input.matches("[0-7]+")) return 0;
-		
-		int pow = input.length() - 1;
-		int sum = 0;
-		for (int i = 0; i < input.length(); i++) {
-			sum += getCharValue(input.charAt(i)) * Math.pow(8, pow--);
-		}
-		return sum;
-	}
-	
-	private static int getCharValue(char c) {
-		if (Character.isDigit(c)) return Character.getNumericValue(c);
-		else return c - 55;
+		return input.chars()
+					.map(c -> (char)c - '0')
+					.reduce(0, (result, c) -> result * 8 + c);
 	}
 }
