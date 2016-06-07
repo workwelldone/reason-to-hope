15a247a6d6b8173125fd0174f98972c4980d9a71
Darren Pearson <darrenpearson@comcast.net>
2016-06-07 14:31:02 -0600

    Updated to use of streams.

--- Hexadecimal.java
+++ Hexadecimal.java
@@ -2,18 +2,10 @@ public class Hexadecimal {
 
 	public static int toDecimal(String input) {
 		input = input.toUpperCase();
-		if (!input.equals(input.replaceAll("[^0-9A-F]", ""))) return 0;
-		
-		int pow = input.length() - 1;
-		int sum = 0;
-		for (int i = 0; i < input.length(); i++) {
-			sum += getCharValue(input.charAt(i)) * Math.pow(16, pow--);
-		}
-		return sum;
-	}
-	
-	private static int getCharValue(char c) {
-		if (Character.isDigit(c)) return Character.getNumericValue(c);
-		else return c - 55; // char value of A is 65
+		if (!input.matches("[0-9A-F]+")) return 0;
+		return input.chars()
+					.mapToObj(c -> (char)c)
+					.map(c -> (c >= 'A') ? c - 'A' + 10  : c - '0')
+					.reduce(0, (result, c) -> result * 16 + c);
 	}
 }
