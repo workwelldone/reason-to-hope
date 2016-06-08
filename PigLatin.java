b07e35c2e902c284972071d496b9a10efee810ce
Darren Pearson <darrenpearson@comcast.net>
2016-06-08 00:17:50 -0600

    Adjusted regex and order of tests

--- PigLatin.java
+++ PigLatin.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 public class PigLatin {
 
-	private static final String VOWELS = "[aeiou].*|[yt].*|[xr].*";
+	private static final String VOWELS = "[aeiou].*|yt.*|xr.*";
 	private static final String DOUBLE_CONSONANTS = "ch.*|qu.*|th.*";
 	private static final String TRIPLE_CONSONANTS = ".qu.*|thr.*|sch.*";
 	
@@ -14,8 +14,8 @@ public class PigLatin {
 
 	private static String translateWord(String word) {
 		if (word.matches(VOWELS)) return word + "ay";
-		if (word.matches(DOUBLE_CONSONANTS)) return word.substring(2) + word.substring(0,1) + "ay";
-		if (word.matches(TRIPLE_CONSONANTS)) return word.substring(3) + word.substring(0,2) + "ay";
+		if (word.matches(TRIPLE_CONSONANTS)) return word.substring(3) + word.substring(0,3) + "ay";
+		if (word.matches(DOUBLE_CONSONANTS)) return word.substring(2) + word.substring(0,2) + "ay";
 		return word.substring(1) + word.charAt(0) + "ay";
 	}
 }
